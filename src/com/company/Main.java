03c3769f91878088fbfa4ecf64d00247224310ac
MGreco2112 <mgreco2112@tutanota.com>
2021-07-26 20:54:11 -0400

    Plan of attack created

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    /*
+    Plan of Attack:
+
+    Classes   Fields/Methods
+
+    Player    (String name, int score, Cup cup);
+    Table     (Map<Die, Integer> tableDice);
+    Game      (int numberOfPlayers, List<Player> players, Player activePlayer, Player lastBetter,
+               bet(Die dieValue, int quantity), callLie(Player lastBetter));
+    Cup       (List<Die> dicePool, addDice(int numOfDice), removeDie(), rollDice());
+    Die       (int numberOfSides, int faceUpValue, roll(), getFaceUpValue);
+
+
+
+     */
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
