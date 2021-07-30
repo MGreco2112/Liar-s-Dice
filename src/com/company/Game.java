6d2bd43b3545cd9974e1dd70c49318dc1e5ce33b
MGreco2112 <mgreco2112@tutanota.com>
2021-07-30 06:52:42 -0400

    Fully Working Game

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
 
 public class Game {
     public int numberOfPlayers;
-    public int betDieValue;
-    public int betNumberOfDice;
+    public int betDieValue = 0;
+    public int betNumberOfDice = 0;
+    private int choice = 0;
     public boolean gameIsActive = true;
     public Player activePlayer;
     public Player lastBetter;
@@ -32,7 +33,6 @@ public class Game {
         } while (playerNumber < 2 && diceNumber < 3);
 
 
-
         while (players.size() < playerNumber) {
             scanner.nextLine();
             String name = createPlayer();
@@ -42,7 +42,7 @@ public class Game {
             }
 
             players.add(new Player(name, diceNumber));
-            System.out.println(name + " has been added to the game");
+            System.out.println(name + " has been added to the game. Press Enter to continue...");
 
         }
 
@@ -66,12 +66,12 @@ public class Game {
         String newGame = scanner.nextLine();
 
         switch (newGame.toLowerCase(Locale.ROOT)) {
-            case "y" :
+            case "y":
                 System.out.println("New Game initiated!");
                 new Game();
                 break;
 
-            case "n" :
+            case "n":
                 System.out.println("Thank you for playing!");
                 System.exit(0);
         }
@@ -93,21 +93,20 @@ public class Game {
             return "End of Game";
         }
         for (Player player : players) {
-            player.cup.rollDice();
+            if (betDieValue == 0) {
+                player.cup.rollDice();
 
-            for (Die die : player.cup.dice) {
-                if (table.tableDice.containsKey(die.faceUpValue)) {
-                    int dieCount = table.tableDice.get(die.faceUpValue);
-                    dieCount++;
-                    table.tableDice.put(die.faceUpValue, dieCount);
-                } else {
-                    table.tableDice.put(die.faceUpValue, 1);
+                for (Die die : player.cup.dice) {
+                    if (table.tableDice.containsKey(die.faceUpValue)) {
+                        int dieCount = table.tableDice.get(die.faceUpValue);
+                        dieCount++;
+                        table.tableDice.put(die.faceUpValue, dieCount);
+                    } else {
+                        table.tableDice.put(die.faceUpValue, 1);
+                    }
                 }
             }
         }
-
-
-        lastBetter = null;
 
 
         for (Player player : players) {
@@ -118,7 +117,7 @@ public class Game {
                 for (Die die : activePlayer.cup.dice) {
                     System.out.println(die.faceUpValue);
                 }
-                if (lastBetter == null) {
+                if (betDieValue == 0) {
                     System.out.println(activePlayer.name + ", what will your bet be?\nEnter a die value: \n");
                     int dieValue = scanner.nextInt();
                     System.out.println("Enter a quantity for that die: \n");
@@ -127,25 +126,26 @@ public class Game {
                     System.out.println(activePlayer.name + " says there are " + betNumberOfDice + " " + betDieValue + "'s" +
                             " on the table.\nNext turn!");
                 } else {
-                    System.out.println(activePlayer.name + ", would you like to \n(1) make a new bet, or \n(2) call " + lastBetter.name + " a liar?\nLast bet: " + betNumberOfDice + " die/dice of " + betDieValue + "'s");
-                    int choice = scanner.nextInt();
+
+                    do {
+                        System.out.println(activePlayer.name + ", would you like to \n(1) make a new bet, or \n(2) call " + lastBetter.name + " a liar?\nLast bet: " + betNumberOfDice + " die/dice of " + betDieValue + "'s");
+                        choice = scanner.nextInt();
+                    } while (choice <= 0 || choice > 2);
 
                     switch (choice) {
                         //TODO Refine this into a valid/invalid betting system
                         case 1:
-                            System.out.println(activePlayer.name + ", what will your bet be?\nEnter a die value: \n");
-                            int dieValue = scanner.nextInt();
-                            System.out.println("Enter a quantity for that die: \n");
-                            int quantity = scanner.nextInt();
-                            bet(dieValue, quantity);
-                            System.out.println(activePlayer.name + " says there are " + betNumberOfDice + " " + betDieValue + "'s" +
-                                    " on the table.\nNext turn!");
+                            secondBet();
                             break;
 
                         case 2:
                             System.out.println(activePlayer.name + " claims " + lastBetter.name + " is lying...");
                             callLiar();
                             round();
+                            break;
+
+                        default:
+                            System.out.println("Invalid selection.");
                     }
                 }
             }
@@ -170,6 +170,65 @@ public class Game {
         betNumberOfDice = numberOfDice;
     }
 
+    public void secondBet() {
+        int newBidValue;
+        int newQuantity;
+        String choice = "";
+
+        if (betDieValue != 6) {
+
+            System.out.println(activePlayer.name + ", make your second bid. You may either:\n1) Increase the die value from the last " +
+                    "bid\n2) Bid any value of dice at an increased quantity");
+            scanner.nextLine();
+            choice = scanner.nextLine();
+        } else {
+            System.out.println(activePlayer.name + ", since the current bid value is 6, you may only increase the " +
+                    "quantity and bid any value");
+            choice = "2";
+        }
+
+
+        switch (choice) {
+            case "1":
+                System.out.println("The last bid was a value of " + betDieValue + ", that occurred " + betNumberOfDice +
+                        " times");
+
+                do {
+                    System.out.println("What value would you like to increase the bid die to?\nOld value: " + betDieValue);
+                    newBidValue = scanner.nextInt();
+                } while (newBidValue <= betDieValue);
+
+                System.out.println("The new bid is " + betNumberOfDice + " instances of " + newBidValue);
+
+                betDieValue = newBidValue;
+                break;
+
+            case "2":
+                do {
+                    System.out.println("Which quantity of dice do you wish to bid? This value must be higher than " + betNumberOfDice);
+                    newQuantity = scanner.nextInt();
+                } while (newQuantity <= betNumberOfDice);
+
+                System.out.println("What die value would you like to bid? This can be of any value");
+
+                newBidValue = scanner.nextInt();
+
+
+                System.out.println("The new bid is " + newQuantity + " instances of " + newBidValue);
+
+                betDieValue = newBidValue;
+                betNumberOfDice = newQuantity;
+                break;
+
+            default:
+                System.out.println("Invalid selection");
+
+        }
+
+        lastBetter = activePlayer;
+
+    }
+
     public void callLiar() {
         if (table.tableDice.containsKey(betDieValue) && table.tableDice.get(betDieValue) >= betNumberOfDice) {
             System.out.println(showTableDice());
@@ -192,6 +251,10 @@ public class Game {
                 checkGameStatus();
             }
         }
+
+        betDieValue = 0;
+        betNumberOfDice = 0;
+        table.tableDice.clear();
     }
 
 
@@ -217,5 +280,6 @@ public class Game {
 
         return tableDice;
     }
+
 
 }
